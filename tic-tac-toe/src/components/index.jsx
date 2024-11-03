6a9a03f78a715d3754b4c6c68696ae3154f08afc
firoliyos <firatesfaye44@gmail.com>
2024-11-03 05:43:26 +0300

    x -o-works

--- tic-tac-toe/src/components/index.jsx
+++ tic-tac-toe/src/components/index.jsx
@@ -1,28 +1,36 @@
+import { useState } from 'react'
 import './styles.css'
 
-function Square({ value }){
+function Square({ value, onClick }){
     return (
-        <button className="square">{value}</button>
+        <button onClick={onClick} className="square">{value}</button>
     )
 }
 export default function TicTacToe() {
-    
+    const [squares, setSquares] = useState(Array(9).fill(''))
+    const [isXTurn, setIsXTurn] = useState(true)
+    function handleClick(getCurrentSquare) {
+        let cpySquares = [...squares]
+        cpySquares[getCurrentSquare] = isXTurn ? 'X' : 'O'
+        setIsXTurn(!isXTurn)
+        setSquares(cpySquares)
+    }
     return(
         <div className="tic-tac-toe-container">
             <div className="row">
-               <Square />
-               <Square />
-               <Square />
+               <Square value={squares[0]} onClick={() => handleClick(0)} />
+               <Square value={squares[1]} onClick={() => handleClick(1)} />
+               <Square value={squares[2]} onClick={() => handleClick(2)} />
             </div>
             <div className="row">
-               <Square />
-               <Square />
-               <Square />
+               <Square value={squares[3]} onClick={() => handleClick(3)} />
+               <Square value={squares[4]} onClick={() => handleClick(4)} />
+               <Square value={squares[5]} onClick={() => handleClick(5)} />
             </div>
             <div className="row">
-               <Square />
-               <Square />
-               <Square />
+               <Square value={squares[6]} onClick={() => handleClick(6)} />
+               <Square value={squares[7]} onClick={() => handleClick(7)} />
+               <Square value={squares[8]} onClick={() => handleClick(8)} />
             </div>
         </div>
     )
