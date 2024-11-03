24ad49a158b6f649428a235071f4518fd8d57fe6
firoliyos <firatesfaye44@gmail.com>
2024-11-03 05:56:11 +0300

    winner-calculator

--- tic-tac-toe/src/components/index.jsx
+++ tic-tac-toe/src/components/index.jsx
@@ -11,6 +11,26 @@ export default function TicTacToe() {
     const [isXTurn, setIsXTurn] = useState(true)
 
 
+    function getWinner(squares) {
+        const winningPatterns = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for(let i=0; i<winningPatterns.length; i++) {
+            const [x,y,z] = winningPatterns[i]
+
+            if(squares[x] && squares[x] === squares[y] && squares[x] === squares[z]) {
+                return squares[x]
+            }
+        }
+        return null
+    }
     function handleClick(getCurrentSquare) {
         let cpySquares = [...squares]
         if(cpySquares[getCurrentSquare]) return
